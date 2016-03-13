965bb553de7396719d2d5300816adf653ac51e13
Thiago Porciúncula <tfcporciuncula@gmail.com>
2016-03-13 17:26:26 -0300

    The map now starts at the current location

--- app/src/main/java/com/arctouch/easybus/map/MapsActivity.java
+++ app/src/main/java/com/arctouch/easybus/map/MapsActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Handler;
 import android.os.ResultReceiver;
 import android.support.v4.app.ActivityCompat;
@@ -13,7 +14,10 @@ import android.support.v7.app.AppCompatActivity;
 import com.arctouch.easybus.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -24,17 +28,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * Activity that holds and manages the map.
  */
-// TODO: start map at current location?
-// TODO: keep map state?
 @SuppressWarnings("ResourceType")
 public class MapsActivity extends AppCompatActivity
         implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 10;
+    private static final int CURRENT_LOCATION_ZOOM = 16;
 
     private static final String KEY_CURRENT_STREET = "currentStreet";
     private static final String EXTRA_STREET_NAME = "com.arctouch.easybus.street_name";
 
     private GoogleMap map;
-//    private GoogleApiClient googleApiClient;
+    private GoogleApiClient googleApiClient;
 
     private String currentStreet;
 
@@ -51,11 +56,11 @@ public class MapsActivity extends AppCompatActivity
             currentStreet = savedInstanceState.getString(KEY_CURRENT_STREET);
         }
 
-//        googleApiClient = new GoogleApiClient.Builder(this)
-//                .enableAutoManage(this, this)
-//                .addConnectionCallbacks(this)
-//                .addApi(LocationServices.API)
-//                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.setRetainInstance(true);
@@ -70,7 +75,61 @@ public class MapsActivity extends AppCompatActivity
 
     @Override
     public void onConnected(Bundle bundle) {
+        if (currentStreet == null) {
+            zoomOnCurrentLocationAndAddStreetMark();
+        }
+    }
 
+    private void zoomOnCurrentLocationAndAddStreetMark() {
+        if (hasLocationAccessPermission()) {
+//            Location lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+//            if (lastKnownLocation != null) {
+//                zoomOnLocationAndAddStreetMark(lastKnownLocation);
+//                return;
+//            }
+            LocationRequest request = buildCurrentLocationRequest();
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, request, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    zoomOnLocationAndAddStreetMark(location);
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION);
+        }
+    }
+
+    private boolean hasLocationAccessPermission() {
+        return ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private LocationRequest buildCurrentLocationRequest() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        return request;
+    }
+
+    private void zoomOnLocationAndAddStreetMark(Location location) {
+        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        addNewStreetMarker(currentLocation);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, CURRENT_LOCATION_ZOOM));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED) ||
+                    (grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                zoomOnCurrentLocationAndAddStreetMark();
+            }
+        }
     }
 
     @Override
@@ -96,13 +155,6 @@ public class MapsActivity extends AppCompatActivity
                 addNewStreetMarker(latLng);
             }
         });
-    }
-
-    private boolean hasLocationAccessPermission() {
-        return ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
 
     private void addNewStreetMarker(final LatLng latLng) {
